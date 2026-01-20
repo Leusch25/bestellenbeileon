b46b612bc469b8afb3b38d83903966ff0a40c522
Leusch25 <l.grill@hgs-singen.online>
2026-01-20 09:11:15 +0000

    added picture and updated script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function loadProducts() {
     const products = [
         { id: 1, name: 'Armband', price: 5.00, image: 'https://via.placeholder.com/200x150?text=Armband' },
         { id: 2, name: 'Schlüsselanhänger Stoff', price: 3.00, image: 'https://files.fm/u/ygtedbra7v' },
-        { id: 3, name: 'Schlüsselanhänger Paracord', price: 5.00, image: 'https://files.fm/u/agn8rfy639' },
+        { id: 3, name: 'Schlüsselanhänger Paracord', price: 5.00, image: '/Paracord.jpg' },
     ];
 
     const productContainer = document.getElementById('products');
